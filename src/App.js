7ece698d6b1c6cfc29d8c0cc95fa84566a6244c6
Farman Ullah <167885990+FarmanCs@users.noreply.github.com>
2025-07-14 01:45:31 +0500

    tour list to get item

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Passports", quantity: 2, packed: false },
+  { id: 4, description: "Socks", quantity: 12, packed: true },
+];
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo />
+      <Form />
+      <Packinglist />
+      <Stats />
+    </div>
+  );
+}
+function Logo() {
+  return <h1>😎 Far away⛷👁</h1>;
+}
+function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+  function handleSubmite(e) {
+    e.preventDefault();
+    // console.log(e);
+    if (!description) return;
+    const newItem = { description, quantity, package: false, id: Date.now() };
+    console.log(newItem);
+    setDescription("");
+    setQuantity(1);
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmite}>
+      <h3>What do you need for you Trip?</h3>
+      <select
+        value={quantity}
+        onChange={(q) => setQuantity(Number(q.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((val) => (
+          <option value={val} key={val}>
+            {val}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="item..."
+        value={description}
+        onChange={(e) => {
+          // console.log(e.target);
+          setDescription(e.target.value);
+        }}
+      />
+      <button>add</button>
+    </form>
+  );
+}
+function Packinglist() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
     </div>
   );
 }
 
-export default App;
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button className="btn">&times;</button>
+    </li>
+  );
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>You have X item on you list and you already pick X (X%)</em>
+    </footer>
+  );
+}
